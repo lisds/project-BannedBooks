4b16f2499f93f4ed55961e4afebe2c2578ef73a2
Jasper Kaucky <jasperkaucky@icloud.com>
2023-12-20 02:39:11 +0100

    Completed Investigating Priors Extension Section, Started NLP

--- projtools/imports.py
+++ projtools/imports.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib import rcParams
+from matplotlib.colors import LogNorm
 import statsmodels.formula.api as smf
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
